69dec4f410db284b9bd042eed62fc3dfa52d45a1
ihtblwFIZ <vthyrachel@naver.com>
2026-02-02 23:10:56 +0900

    feat: add SwaggerConfig

---
+++ src/main/java/sejong/alom/teammate/global/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package sejong.alom.teammate.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.title("팀메이트 API");
+
+		return new OpenAPI()
+			.info(info);
+	}
+}
