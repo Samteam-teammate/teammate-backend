a52af86b0b850c663c25d7896ecaadc0204a718b
ihtblwFIZ <vthyrachel@naver.com>
2026-02-14 19:58:54 +0900

    feat: add team member

--- src/main/java/sejong/alom/teammate/domain/team/dto/TeamMemberUpdateRequest.java
+++ src/main/java/sejong/alom/teammate/domain/team/dto/TeamMemberUpdateRequest.java
@@ -3,8 +3,8 @@ package sejong.alom.teammate.domain.team.dto;
 import jakarta.validation.constraints.NotNull;
 import sejong.alom.teammate.global.enums.Part;
 
-public record TeamMemberPartUpdateRequest(
-	@NotNull(message = "변경할 역할을 선택해주세요.")
+public record TeamMemberUpdateRequest(
+	@NotNull(message = "역할을 선택해주세요.")
 	Part part
 ) {
 }
