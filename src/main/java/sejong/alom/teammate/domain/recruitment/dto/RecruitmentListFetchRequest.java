f3eab229e0391f624fda8c0674feddc2b788c977
ihtblwFIZ <vthyrachel@naver.com>
2026-02-16 19:45:36 +0900

    feat: fetch home content

--- src/main/java/sejong/alom/teammate/domain/recruitment/dto/RecruitmentListFetchRequest.java
+++ src/main/java/sejong/alom/teammate/domain/recruitment/dto/RecruitmentListFetchRequest.java
@@ -2,10 +2,12 @@ package sejong.alom.teammate.domain.recruitment.dto;
 
 import java.util.List;
 
+import lombok.Builder;
 import sejong.alom.teammate.global.enums.Part;
 import sejong.alom.teammate.global.enums.SortingType;
 import sejong.alom.teammate.global.enums.TeamCategory;
 
+@Builder
 public record RecruitmentListFetchRequest(
 	SortingType sort,
 	List<TeamCategory> categories,
