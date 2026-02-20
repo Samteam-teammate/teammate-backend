1b8504d83b00b0b8f951a05614b1d9bec7414696
ihtblwFIZ <vthyrachel@naver.com>
2026-02-21 00:06:42 +0900

    feat: fetch applicant list

--- src/main/java/sejong/alom/teammate/domain/recruitment/repository/ApplyRepository.java
+++ src/main/java/sejong/alom/teammate/domain/recruitment/repository/ApplyRepository.java
@@ -1,9 +1,16 @@
 package sejong.alom.teammate.domain.recruitment.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import sejong.alom.teammate.domain.recruitment.entity.Apply;
 
 public interface ApplyRepository extends JpaRepository<Apply, Long> {
 	Boolean existsByRecruitmentIdAndMemberId(Long recruitmentId, Long memberId);
+
+	@Query(value = "select a from Apply a join fetch a.member where a.recruitment.id = :recruitmentId",
+		countQuery = "select count(a) from Apply a where a.recruitment.id = :recruitmentId")
+	Page<Apply> findAllByRecruitmentIdWithMember(Long recruitmentId, Pageable pageable);
 }
