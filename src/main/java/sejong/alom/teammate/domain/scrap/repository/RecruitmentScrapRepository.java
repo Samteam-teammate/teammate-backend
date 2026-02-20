22ef6a976c3f3dfbc78930b4f7b9b7ba2f245466
ihtblwFIZ <vthyrachel@naver.com>
2026-02-20 23:02:46 +0900

    feat: unscrap recruitment

--- src/main/java/sejong/alom/teammate/domain/scrap/repository/RecruitmentScrapRepository.java
+++ src/main/java/sejong/alom/teammate/domain/scrap/repository/RecruitmentScrapRepository.java
@@ -1,9 +1,12 @@
 package sejong.alom.teammate.domain.scrap.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sejong.alom.teammate.domain.scrap.entity.RecruitmentScrap;
 
 public interface RecruitmentScrapRepository extends JpaRepository<RecruitmentScrap, Long> {
 	boolean existsByRecruitmentIdAndMemberId(Long teamId, Long memberId);
+	Optional<RecruitmentScrap> findByRecruitmentIdAndMemberId(Long teamId, Long memberId);
 }
